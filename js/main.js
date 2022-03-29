3f10568d0d4472b7cf90c76c61694b6cf734648c
Ксения <ksenia@MacBook-Pro-Nick.local>
2022-03-29 19:57:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+$('.phone').mask('+38(099) 999-99-99');
+
+$('.burger').on('click', function () {
+  $(this).toggleClass('active');
+  $('.header-nav').toggleClass('active')
+})
+
+
+
+// Анимация WOW
+var wow = new WOW({
+  mobile: false,
+
+});
+wow.init();
+
+
+
+
+
+
+
+
+
+
