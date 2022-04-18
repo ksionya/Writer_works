6b085bc37f88ab9d41fe6313d7919665c4523fd3
Ксения <ksenia@MacBook-Pro-Nick.local>
2022-04-18 13:42:19 +0300

    fix 18.04.22

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
+function phoneMask() {
+    $('.phone').mask('+38(099) 999-99-99');
+}
 
 
-$('.phone').mask('+38(099) 999-99-99');
+function mobileMenu() {
 
-$('.burger').on('click', function () {
-  $(this).toggleClass('active');
-  $('.header-nav').toggleClass('active')
-})
+    $('.burger').on('click', function() {
+        $(this).toggleClass('active');
+        $('.header-nav').toggleClass('active');
+        $('body').toggleClass('js-noscroll');
+    });
+
+    $('body').on('click', function(e) {
+
+        if ($(e.target).hasClass('header-nav')) {
+          $('.burger').removeClass('active');
+            $('.header-nav').removeClass('active');
+          $('body').removeClass('js-noscroll');
+        }
+
+    })
+};
+
+
+
 
 
 
 // Анимация WOW
 var wow = new WOW({
-  mobile: false,
+    mobile: false,
 
 });
 wow.init();
 
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    phoneMask();
+    mobileMenu();
+});
